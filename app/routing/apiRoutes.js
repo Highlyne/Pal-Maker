5540d19374480f4833a848fb7995bf577b0179fc
Tamara Hall <tamarahall@Tamaras-MBP.lan>
2018-05-03 21:15:22 -0400

    adding starting content to all files

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,5 +1,27 @@
-// The `apiRoutes.js` file should contain two routes:
 
-//    * A GET route with the url `/api/friends`. This will be used to display a JSON of all possible friends.
-//    * A POST routes `/api/friends`. This will be used to handle incoming survey results. 
-// This route will also be used to handle the compatibility logic. 
+// Routes
+// =============================================================
+
+
+ // A GET route with the url `/api/friends` that displays JSON object of all friends
+ app.get("/api/friends", function(req, res) {
+    return res.json(characters);
+  });
+
+  
+  // A POST route to `/api/friends`. This will be used to handle incoming survey results. 
+  app.post("/api/friends", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body-parser middleware
+    var newFriend = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newFriend.routeName = newFriend.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newFriend);
+  
+    characters.push(newFriend);
+  
+    res.json(newFriend);
+  });
